5367ed3e488464a8d7884cfdb48210b33b1910b5
Nikola <nikola.r.totev@gmail.com>
2019-12-26 23:27:00 +0200

    Added initial UndirectedGraphStructure

---
+++ CS_Data_Structures/CustomStructures/UndirectedGraph.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomStructures
+{
+    public class UndirectedGraph<T>
+    {
+        public List<UndirectedGraphNode<T>> AdjecencyList { get; set; }
+
+        public UndirectedGraph(T initialNodeData)
+        {
+            UndirectedGraphNode<T> initialNode = new UndirectedGraphNode<T>(initialNodeData);
+            AdjecencyList.Add(initialNode);
+        }
+
+        public UndirectedGraph(UndirectedGraphNode<T> initialNode)
+        {
+            AdjecencyList.Add(initialNode);
+        }
+
+        public void AddEdge(UndirectedGraphNode<T> vertexOne, UndirectedGraphNode<T> vertexTwo)
+        {
+            int v1Index = -1;
+            int v2Index = -1;
+
+            if (AdjecencyList.Contains(vertexOne))
+            {
+                v1Index = AdjecencyList.IndexOf(vertexOne);
+            }
+
+            if (AdjecencyList.Contains(vertexTwo))
+            {
+                v2Index = AdjecencyList.IndexOf(vertexTwo);
+            }
+
+            if (v1Index == -1 && v2Index != -1)
+            {
+                AdjecencyList.Add(vertexOne);
+                vertexTwo.AddNeighbor(vertexOne);
+                return;
+            }
+
+            if (v2Index == -1 && v1Index != -1)
+            {
+                AdjecencyList.Add(vertexTwo);
+                vertexOne.AddNeighbor(vertexTwo);
+                return;
+            }
+
+            if (v1Index == -1 && v2Index == -1)
+            {
+                AdjecencyList.Add(vertexOne);
+                AdjecencyList.Add(vertexTwo);
+
+                vertexOne.AddNeighbor(vertexTwo);
+                vertexTwo.AddNeighbor(vertexOne);
+                return;
+            }
+
+            if (v1Index != -1 && v2Index != -1)
+            {
+                if (!vertexOne.Neighbors.Contains(vertexTwo))
+                {
+                    vertexOne.AddNeighbor(vertexTwo);
+                }
+                if (!vertexTwo.Neighbors.Contains(vertexOne))
+                {
+                    vertexTwo.AddNeighbor(vertexOne);
+                }
+            }
+        }
+    }
+}
---
+++ CS_Data_Structures/CustomStructures/UndirectedGraphNode.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomStructures
+{
+    public class UndirectedGraphNode<T>
+    {
+        public List<UndirectedGraphNode<T>> Neighbors { get; set; }
+        public T Data { get; set; }
+
+        public UndirectedGraphNode(T data)
+        {
+            Neighbors = new List<UndirectedGraphNode<T>>();
+            Data = data;
+        }
+
+        public UndirectedGraphNode(UndirectedGraphNode<T> initialNeighbor, T data)
+        {
+            Neighbors = new List<UndirectedGraphNode<T>>();
+            Neighbors.Add(initialNeighbor);
+            initialNeighbor.Neighbors.Add(this);
+            Data = data;
+        }
+
+        public void AddNeighbor(UndirectedGraphNode<T> neighborToAdd)
+        {
+            Neighbors.Add(neighborToAdd);
+        }
+
+        public void RemoveNeighbor(UndirectedGraphNode<T> neighborToRemove)
+        {
+            Neighbors.Remove(neighborToRemove);
+        }
+    }
+}
