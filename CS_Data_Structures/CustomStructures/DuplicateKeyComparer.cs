482368a3e4dedd4ef76f930ba65dbf3d0861b889
Nikola <nikola.r.totev@gmail.com>
2020-01-02 02:32:28 +0200

    Fixed DuplicateKeyComparer

--- CS_Data_Structures/CustomStructures/DuplicateKeyComparer.cs
+++ CS_Data_Structures/CustomStructures/DuplicateKeyComparer.cs
@@ -3,30 +3,28 @@ using System.Collections.Generic;
 
 namespace CustomStructures
 {
-    public class DuplicateKeyComparer
+
+    /// <summary>
+    /// Comparer for comparing two keys, handling equality as beeing greater
+    /// Use this Comparer e.g. with SortedLists or SortedDictionaries, that don't allow duplicate keys
+    /// </summary>
+    /// <typeparam name="TKey"></typeparam>
+    public class DuplicateKeyComparer<TKey>
+        :
+            IComparer<TKey> where TKey : IComparable
     {
-        /// <summary>
-        /// Comparer for comparing two keys, handling equality as beeing greater
-        /// Use this Comparer e.g. with SortedLists or SortedDictionaries, that don't allow duplicate keys
-        /// </summary>
-        /// <typeparam name="TKey"></typeparam>
-        public class DuplicateKeyComparer<TKey>
-            :
-                IComparer<TKey> where TKey : IComparable
+        #region IComparer<TKey> Members
+
+        public int Compare(TKey x, TKey y)
         {
-            #region IComparer<TKey> Members
+            int result = x.CompareTo(y);
 
-            public int Compare(TKey x, TKey y)
-            {
-                int result = x.CompareTo(y);
-
-                if (result == 0)
-                    return 1;   // Handle equality as beeing greater
-                else
-                    return result;
-            }
-
-            #endregion
+            if (result == 0)
+                return 1;   // Handle equality as beeing greater
+            else
+                return result;
         }
-    
+
+        #endregion
+    }
 }
